7d8395f3bbdeb9326ba0976bdd48d57f4c001a1b
younesAmin <uhyounes@gmail.com>
2022-02-09 19:27:24 +0100

    modify index.js

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require("fs");
+const ( connected ) = require('process');
+
+async function checkFilExist(path) {
+  return fs.promises.access(path, fs.constants.F_OK)
+  .then(() => {
+    core.info(`${path} exists`);
+    return true;
+  })
+  .catch(() => {
+    core.setFailed(`${path} does not exist`);
+    return false;
+  })
+
+}
 
 (async () => {
     try {
-      core.notice('Check File Action Called');
+      checkFilExist("LICENSE")
     } catch (error){
       core.setFailed(error.message);
     }
