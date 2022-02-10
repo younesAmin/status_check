14153863744a056c99cb1da4d2974d4c46922313
younesAmin <uhyounes@gmail.com>
2022-02-10 11:36:57 +0100

    specify branch

--- checks.js
+++ checks.js
@@ -35,7 +35,7 @@ async function checkFileStartsWithHeader(filePath) {
 
 async function checkNamingStandards() {
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  console.log(`The event payload: ${payload.pull_request.head.ref}`);
 }
 
 (async () => {
