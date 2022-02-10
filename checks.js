91cb92a0f2bcf83fe7e0f577233ef2477092f875
younesAmin <uhyounes@gmail.com>
2022-02-10 12:20:07 +0100

    add annotation

--- checks.js
+++ checks.js
@@ -34,8 +34,9 @@ async function checkFileStartsWithHeader(filePath) {
 }
 
 async function checkNamingStandards() {
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload.pull_request.head.ref}`);
+  const payload = JSON.stringify(github.context.payload.pull_request.head.ref, undefined, 2)
+
+  console.log(`The event payload: ${payload}`);
 }
 
 (async () => {
@@ -44,7 +45,41 @@ async function checkNamingStandards() {
       checkFilExist("README.md")
       checkFilExist("CONTRIBUTING.md")
       checkFilExist("SECURITY.md")
-      checkFileStartsWithHeader("README.md");
+      // checkFileStartsWithHeader("README.md");
+      if (
+          ! await checkFileStartsWithHeader("README.md")
+      ) {
+          // get token for octokit
+          const token = core.getInput('repo-token');
+          const octokit = new github.getOctokit(token);
+          // call octokit to create a check with annotation and details
+          const check = await octokit.rest.checks.create({
+              owner: github.context.repo.owner,
+              repo: github.context.repo.repo,
+              name: 'Readme Validator',
+              head_sha: github.context.sha,
+              status: 'completed',
+              conclusion: 'failure',
+              output: {
+                  title: 'README.md must start with a title',
+                  summary: 'Please use markdown syntax to create a title',
+                  annotations: [
+                      {
+                          path: 'README.md',
+                          start_line: 1,
+                          end_line: 1,
+                          annotation_level: 'failure',
+                          message: 'README.md must start with a header',
+                          start_column: 1,
+                          end_column: 1
+                      }
+                  ]
+              }
+          });
+      }
+         
+
+
       checkNamingStandards();
     } catch (error){
       core.setFailed(error.message);
