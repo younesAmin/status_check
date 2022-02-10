0910873b77f73342ebe047b7a3b4da7ba63adc40
younesAmin <uhyounes@gmail.com>
2022-02-10 12:58:30 +0100

    see payload 3

--- checks.js
+++ checks.js
@@ -35,7 +35,7 @@ async function checkFileStartsWithHeader(filePath) {
 
 async function checkNamingStandards() {
   // const payload = JSON.stringify(github.context.payload.pull_request.head, undefined, 2)
-  console.log(`The event payload: ${github.context.payload}`);
+  console.log(`The event payload: ${github.context.payload.base}`);
   // if (payload === payload.toLowerCase()){
   //   core.info(`Branch ${payload} is in lowercase`);
   //   return true;
